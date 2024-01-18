40de770ff4143efb4174267a1a2280b7c8723e50
Nick Scoles <scolesnick@gmail.com>
2024-01-18 10:39:16 -0600

    Challenge 1 finished

--- Day7-Hangman/Day7-Challenge1.py
+++ Day7-Hangman/Day7-Challenge1.py
@@ -3,7 +3,16 @@
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+import random as r
+chosen_word = r.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input('Guess a letter: ').lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# video asks to check for a match against each letter in the chosen word
+for c in chosen_word:
+    if guess == c:
+        print('Right')
+    else:
+        print('Wrong')
