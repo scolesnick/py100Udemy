83662f169ab373097a06c7cd69150bcfae44889e
Nick Scoles <scolesnick@gmail.com>
2024-01-18 09:47:04 -0600

    initial commit

---
+++ Day7-Hangman/Day7-InitialAttempt.py
@@ -0,0 +1,9 @@
+# Hangman game
+#   The final script includes ASCII art
+#   This script will just include blanks with a number of guesses
+
+word = 'beekeeper'
+
+word_list = list(word)
+
+print(word_list)
