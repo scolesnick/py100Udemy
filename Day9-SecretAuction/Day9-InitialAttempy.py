e0869013d39ed1486748ebb661db4f8b79792a88
numbscole <scolesnick@gmail.com>
2024-02-03 23:50:40 -0600

    Finished challenge

--- Day9-SecretAuction/Day9-InitialAttempy.py
+++ Day9-SecretAuction/Day9-InitialAttempy.py
@@ -1,7 +1,24 @@
+import os
+
 print('Welcome to the secret auction program.')
-name = input('What is your name?: ')
-bid = input('What is your bid?: $')
 
-others = input('Are there any other bidders? Type \'yes\' or \'no\' \n')
+bidders = {}
 
+others = 'yes'
+while others.lower() == 'yes':
+    name = input('What is your name?: ')
+    bid = float(input('What is your bid?: $'))
+    others = input('Are there any other bidders? Type \'yes\' or \'no\' \n')
+    bidders[name] = bid
+    os.system('cls')
 
+highest = ''
+highest_val = 0.0
+
+# Print the winner (who had the highest bid)
+for k,v in bidders.items():
+    if v > highest_val:
+        highest_val =v
+        highest = k
+
+print(f'The winner is {highest} with a bid of ${highest_val}.')
