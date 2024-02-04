3262ec3bd1497013d8122848e04254f89450ada3
numbscole <scolesnick@gmail.com>
2024-02-03 23:27:06 -0600

    initial push for day9 folder

---
+++ Day9-SecretAuction/Day9-InitialAttempy.py
@@ -0,0 +1,7 @@
+print('Welcome to the secret auction program.')
+name = input('What is your name?: ')
+bid = input('What is your bid?: $')
+
+others = input('Are there any other bidders? Type \'yes\' or \'no\' \n')
+
+
