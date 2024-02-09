ff158d481ffc56166d2266928dc5d8a0ba3dc9b3
numbscole <scolesnick@gmail.com>
2024-02-08 22:29:02 -0600

    Final commit

--- Day15-CoffeeMachine/main.py
+++ Day15-CoffeeMachine/main.py
@@ -1,13 +1,6 @@
 # Make coffee machine that provides 3 recipes
 # Coffee machine is coin operated
 
-# Requirements:
-# TODO  1. Print a report when asked to show resources in machine
-# TODO  2. Check if the resources are sufficient for a selection
-# TODO  3. Process coins entered
-# TODO  4. Check if the transaction was successful
-# TODO  5. Make Coffee (update resources)
-
 import data
 
 resources = data.resources
