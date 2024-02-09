2027904de1f5354fd52301c9de80f2ae6830f27e
numbscole <scolesnick@gmail.com>
2024-02-09 11:49:15 -0600

    Turle Drawing

---
+++ Day16-OOP/TurtleDemo/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+# import another_module as am
+
+# print(am.another_variable)
+
+timmy = Turtle()
+my_screen = Screen()
+
+timmy.shape("turtle")
+timmy.color("chartreuse")
+timmy.forward(100)
+
+print(my_screen.canvheight)
+my_screen.exitonclick()
