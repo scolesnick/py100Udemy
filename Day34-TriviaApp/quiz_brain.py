65a784e612c36d87d8b562b1f8f4a9bf24333183
Nick Scoles <scolesnick@gmail.com>
2024-10-03 18:15:11 -0500

    Revert "Quiz app completed"
    This reverts commit 4ca72265c2a1e0c00c131fbc6b2c5922c53873b6.

--- Day34-TriviaApp/quiz_brain.py
+++ Day34-TriviaApp/quiz_brain.py
@@ -19,10 +19,13 @@ class QuizBrain:
         # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
         # self.check_answer(user_answer)
 
-    def check_answer(self, user_answer: str):
+    def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            return True
+            print("You got it right!")
         else:
-            return False
+            print("That's wrong.")
+
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
