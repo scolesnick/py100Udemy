dd514d652aaecdd89081e27cb7801f7e8808ea4c
Nick Scoles <scolesnick@gmail.com>
2024-10-05 15:20:12 -0500

    print notification that it will rain today

--- Day35-SMS/rainalert.py
+++ Day35-SMS/rainalert.py
@@ -1,15 +1,17 @@
 import requests
 
 
-MY_LAT = 35.075500 # Your latitude
-MY_LONG = -91.881752 # Your longitude
+MY_LAT = 30.332184
+MY_LONG = -81.655647
+# MY_LAT = 35.075500
+# MY_LONG = -91.881752
 APP_ID = ''
-
 
 params = {
     'lat': MY_LAT,
     'lon': MY_LONG,
     'appid': APP_ID,
+    'cnt': 4,
     'units': 'Imperial'
 }
 
@@ -18,5 +20,12 @@ response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", 
 response.raise_for_status()
 data = response.json()
 
-print(response.status_code)
-print(data)
+# print(response.status_code)
+# print(data)
+
+rain = False
+for e in data['list']:
+    if e['weather'][0]['id'] < 700:
+        rain = True
+        break
+print(f'It will rain: {rain}')
