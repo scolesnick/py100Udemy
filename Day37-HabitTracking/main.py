d910bef1380c3a86dfd0a6978ab0d9fc147a4a3d
Nick Scoles <scolesnick@gmail.com>
2024-10-05 20:48:03 -0500

    created graph with pixela

--- Day37-HabitTracking/main.py
+++ Day37-HabitTracking/main.py
@@ -1,14 +1,31 @@
 import requests
-
+USERNAME = ''
+TOKEN = ''
 pixela_endpoint = 'https://pixe.la/v1/users'
 
-user_params = {
-    'token':'',
-    'username':'numbscole',
-    'agreeTermsOfService':'yes',
-    'notMinor':'yes'
-}
-
+# user_params = {
+#     'token': TOKEN,
+#     'username':USERNAME,
+#     'agreeTermsOfService':'yes',
+#     'notMinor':'yes'
+# }
+#
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
+graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
+
+graph_config = {
+    'id':'graph1',
+    'name':'Cycling Graph',
+    'unit':'Km',
+    'type':'float',
+    'color':'ajisai'
+}
+
+headers = {
+    'X-USER-TOKEN':TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
