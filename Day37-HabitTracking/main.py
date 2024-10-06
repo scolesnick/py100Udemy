0fc01be26702c229e1030cff91c744761c585529
Nick Scoles <scolesnick@gmail.com>
2024-10-05 20:22:49 -0500

    create user in pixela

---
+++ Day37-HabitTracking/main.py
@@ -0,0 +1,14 @@
+import requests
+
+pixela_endpoint = 'https://pixe.la/v1/users'
+
+user_params = {
+    'token':'',
+    'username':'numbscole',
+    'agreeTermsOfService':'yes',
+    'notMinor':'yes'
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
