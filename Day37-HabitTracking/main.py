ec60a6d83a5af0b257a82f27fba21f9c4253ef0e
Nick Scoles <scolesnick@gmail.com>
2024-10-06 20:20:24 -0500

    Put and Delete requests added

--- Day37-HabitTracking/main.py
+++ Day37-HabitTracking/main.py
@@ -1,4 +1,6 @@
 import requests
+import datetime
+
 USERNAME = ''
 TOKEN = ''
 pixela_endpoint = 'https://pixe.la/v1/users'
@@ -14,18 +16,41 @@ pixela_endpoint = 'https://pixe.la/v1/users'
 # print(response.text)
 
 graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
-
-graph_config = {
-    'id':'graph1',
-    'name':'Cycling Graph',
-    'unit':'Km',
-    'type':'float',
-    'color':'ajisai'
-}
-
+#
+# graph_config = {
+#     'id':'graph1',
+#     'name':'Cycling Graph',
+#     'unit':'Km',
+#     'type':'float',
+#     'color':'ajisai'
+# }
+#
 headers = {
     'X-USER-TOKEN':TOKEN
 }
+#
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+today = datetime.datetime.now()
+# today = datetime.datetime(year=2024, month=10, day=3)
+add_endpoint = f'{graph_endpoint}/graph1'
+add_config = {
+    'date':today.strftime('%Y%m%d'),
+    'quantity':'5'
+}
+# Adding a pixel
+# response = requests.post(url=add_endpoint, json=add_config, headers=headers)
+
+put_endpoint = f'{add_endpoint}/'+add_config['date']
+put_config = {
+    'quantity':'7'
+}
+# Updating a pixel
+# response = requests.put(url=put_endpoint, json=put_config, headers=headers)
+# print(response.text)
+
+# Deleting a pixel
+delete_endpoint = put_endpoint
+response = requests.delete(url=delete_endpoint, headers=headers)
 print(response.text)
