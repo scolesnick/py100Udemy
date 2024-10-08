962558607f9050fd0ae0d458156763f955b8089b
Nick Scoles <scolesnick@gmail.com>
2024-10-07 19:03:43 -0500

    The language model and google sheet APIs work

---
+++ Day38-WorkoutTracking/main.py
@@ -0,0 +1,48 @@
+import requests
+import sys
+import datetime
+
+APP_ID = sys.argv[1]
+API_KEY = sys.argv[2]
+HOST = 'https://trackapi.nutritionix.com'
+SHEETY_URL = sys.argv[3]
+
+headers = {
+    'x-app-id':APP_ID,
+    'x-app-key':API_KEY
+}
+
+# test getting exercise stats
+post_endpoint = f'{HOST}/v2/natural/exercise'
+
+exercise = input('Tell me which exercises you did: ')
+# exercise = 'I ran for 20 minutes and walked for 2Km.'
+post_params = {
+    'query': exercise
+}
+
+response = requests.post(url=post_endpoint, data=post_params, headers=headers)
+print(response.json())
+
+# For each exercise listed collect the Exercise Name, Duration, Calories Burned, along with the Date and Time
+names = [e['name'][0].upper() + e['name'][1:] for e in response.json()['exercises']]
+durations = [e['duration_min'] for e in response.json()['exercises']]
+cals = [e['nf_calories'] for e in response.json()['exercises']]
+today = datetime.datetime.now()
+date = today.strftime('%d/%m/%Y')
+# now_time = today.time().strftime('%H:%M:%S')
+now_time = today.strftime('%X')
+
+# Connect to Sheety and post the rows
+for i in range(len(names)):
+    entry = {
+        'workout': {
+            'date':date,
+            'time':now_time,
+            'exercise':names[i],
+            'duration':durations[i],
+            'calories':cals[i]
+        }
+    }
+    response_sheety = requests.post(url=SHEETY_URL, json=entry)
+    print(response_sheety.text)
